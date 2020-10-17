3985162a9bb4035053d37cc05f48290b653cfc85
Keith Williams <thestreetkw@hotmail.com>
2020-10-17 02:25:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,9 +20,8 @@ def get_english_meaning(file, emoticon)
   new_library.each do |key, value|
      emo_library = value 
      emo_library.each do |english, japanese|
-binding pry 
-    if emo_library[:english] == emoticon 
-      return emo_library[:japanese]
+    if emo_library[english] == emoticon 
+      return emo_library[japanese]
     else 
       return "Sorry, that emoticon was not found"
 end 
