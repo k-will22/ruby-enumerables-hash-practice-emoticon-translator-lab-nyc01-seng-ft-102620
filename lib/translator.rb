c432c4fdc0901f7c2f04c7505e781601c2487033
Keith Williams <thestreetkw@hotmail.com>
2020-10-17 02:22:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ def get_english_meaning(file, emoticon)
   new_library = load_library(file)
   new_library.each do |key, value|
      emo_library = value 
+     emo_library
     if emo_library[:english] == emoticon 
       return emo_library[:japanese]
     else 
