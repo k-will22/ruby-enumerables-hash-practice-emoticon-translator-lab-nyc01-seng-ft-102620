9b4ba3164ff835e3baa4e8c53455845a0858652a
Keith Williams <thestreetkw@hotmail.com>
2020-10-17 03:37:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,24 +15,7 @@ end
 =begin
 def get_english_meaning(file, emoticon)
   # code goes here
-  new_library = load_library(file)
-  library = YAML.load_file(file)
-  new_library = {"get_meaning" => {},
-                 "get_emoticon" => {}}
-   library.each do |meaning, value| 
-     new_library["get_meaning"][value[1]] = meaning 
-     new_library["get_emoticon"][value[0]] = value[1]
-   end 
-     english_meaning = new_library["get_meaning"]
-    end 
-    
-   value.each do |english, japanese|
-   if english == emoticon 
-     return japanese
-  else 
-    return "Sorry, that emoticon was not found"
-  end
-end 
+  
 end 
 =end 
 def get_japanese_emoticon(file, emoticon)
