b4c12d85847db9c9c4a07e8922a88f1909f95e4c
Keith Williams <thestreetkw@hotmail.com>
2020-10-17 02:17:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_english_meaning(file, emoticon)
   emo_library = {}
   new_library = load_library(file)
   new_library.each do |key, value|
-     emo_library = value 
+     emo_library[key] = value 
      emo_library 
 binding.pry 
 end 
